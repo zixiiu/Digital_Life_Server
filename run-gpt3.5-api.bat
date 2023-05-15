de6637648f8ec3fee36aa84eb098bd979a8b1a5f
seanxpw <1215711652@QQ.com>
2023-05-15 21:13:38 +0800

    删除多余的命令

--- run-gpt3.5-api.bat
+++ run-gpt3.5-api.bat
@@ -4,6 +4,5 @@ PROXY=http://127.0.0.1:7890
 STREAM=False
 CHARACTER=paimon
 MODEL=gpt-3.5-turbo
-echo $SCRIPT_NAME
 
 python $SCRIPT_NAME --chatVer $CHATVER --stream $STREAM --character $CHARACTER --model $MODEL
